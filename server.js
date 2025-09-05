f9222137a9a96c81e103b65f2b6fbf1b370f4f07
clane83 <clane83@yahoo.com>
2025-09-05 15:41:07 -0700

    checkin mongodb connection added to project

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongodb = require('./data/database.js');
+
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use('/', require('./routes'));
+
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log('Connected to MongoDB');
+    }
+})
+
 
 app.listen(port, () => (console.log(`Listening on port ${port}`)));
