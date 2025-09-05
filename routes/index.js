ad18ad13c5030a0d0aa950984923e17cf669d424
clane83 <clane83@yahoo.com>
2025-09-05 16:53:28 -0700

    checkin database loads on main screen

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => { res.send('Hello World'); });
+// router.get('/', (req, res) => { res.send('Hello World'); });
 
-router.use('/contacts', require('./contacts'));
+router.use('/', require('./contacts'));
 
 module.exports = router;
