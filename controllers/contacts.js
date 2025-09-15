22419909ca2f88c0397924f78bb7987c6cb72cf5
clane83 <clane83@yahoo.com>
2025-09-15 09:27:06 -0700

    add error handling

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,21 +3,37 @@ const { ObjectId } = require('mongodb');
 
 const getAll = async (req, res) => {
     //#swagger.tags['Contacts']
-    const result = await mongodb.getDatabase().db().collection('contacts').find();
-    result.toArray().then((contacts) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts);
-    });
+    mongodb
+        .getDatabase()
+        .db()
+        .collection('contacts')
+        .find()
+        .toArray((err, lists) => {
+            if (err) {
+                res.status(400).json({ message: err });
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists);
+        });
+
 };
 
 const getSingle = async (req, res) => {
     //#swagger.tags['Contacts']
-    const contactId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: contactId });
-    result.toArray().then((contacts) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts[0]);
-    });
+    mongodb
+        .getDatabase()
+        .db()
+        .collection('contacts')
+        .find({ _id: contactId })
+        .toArray((err, result) => {
+            if (err) {
+                res.status(400).json({ message: err });
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(result[0]);
+        });
+
+
 };
 
 
